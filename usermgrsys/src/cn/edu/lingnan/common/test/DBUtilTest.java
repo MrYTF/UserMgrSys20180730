da50686907bf9eda2d050ae4fd7681b2e13ea8f7
'MrYTF' <'1534171551@qq.com'>
2018-07-30 10:15:58 +0800

    用户管理系统

---
+++ usermgrsys/src/cn/edu/lingnan/common/test/DBUtilTest.java
@@ -0,0 +1,40 @@
+package cn.edu.lingnan.common.test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import org.junit.Test;
+
+import cn.edu.lingnan.common.util.DBUtil;
+
+public class DBUtilTest {
+	/**
+	 * 数据库连接测试
+	 */
+	@Test	//注解
+	public void testGetconnection() {
+		Connection connection = null;
+		try {
+			connection = DBUtil.getconnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(connection);
+	}
+	
+	/**
+	 * 关闭数据库连接测试
+	 */
+	@Test
+	public void testCloseConnection() {
+		Connection connection = null;
+		try {
+			connection = DBUtil.getconnection();
+			DBUtil.closeConnection(connection);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
